26daa80dc92cd69f06f5efa2a008a35eab4d3c8a
italo.santana <italo.santana@stone.com.br>
2025-08-24 16:58:18 -0300

    refactor: reorder imports in DatabaseModule.ios.kt for consistency

--- composeApp/src/iosMain/kotlin/di/DatabaseModule.ios.kt
+++ composeApp/src/iosMain/kotlin/di/DatabaseModule.ios.kt
@@ -1,10 +1,10 @@
 package di
 
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import com.company.itunes.database.iTunesAlbumsDatabase
 import org.koin.core.module.Module
 import org.koin.dsl.module
-import app.cash.sqldelight.db.SqlDriver
-import app.cash.sqldelight.driver.native.NativeSqliteDriver
 
 actual val databaseModule: Module = module {
     single<SqlDriver> {
