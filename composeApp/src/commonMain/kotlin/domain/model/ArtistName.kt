6045df25f12e374a9aafee9c491eae44ab3cd4cc
italo.santana <italo.santana@stone.com.br>
2025-08-24 01:02:50 -0300

    adjust domain models for data layer integration

--- composeApp/src/commonMain/kotlin/domain/model/ArtistName.kt
+++ composeApp/src/commonMain/kotlin/domain/model/ArtistName.kt
@@ -3,8 +3,14 @@ package domain.model
 import kotlin.jvm.JvmInline
 
 @JvmInline
-value class ArtistName(val value: String) {
+value class ArtistName private constructor(val value: String) {
+
     init {
         require(value.isNotBlank()) { "Artist name cannot be blank" }
+    }
+
+    companion object {
+        fun fromString(value: String): ArtistName = ArtistName(value)
+
     }
 }
--- composeApp/src/commonMain/kotlin/domain/usecase/GetAlbumDetailsUseCase.kt
+++ composeApp/src/commonMain/kotlin/domain/usecase/GetAlbumDetailsUseCase.kt
@@ -3,23 +3,18 @@ package domain.usecase
 import domain.model.Album
 import domain.model.AlbumId
 import domain.model.AppResult
-import domain.repository.AlbumsRepository
+import data.local.AlbumsLocalDataSource
+import domain.model.AlbumError
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class GetAlbumDetailsUseCase(
-    private val repository: AlbumsRepository
+    private val localDataSource: AlbumsLocalDataSource
 ) {
-    operator fun invoke(albumId: AlbumId): Flow<AppResult<Album?>> =
-        repository.getTopAlbums().map { result ->
-            when (result) {
-                is AppResult.Success -> {
-                    val album = result.data.find { it.id == albumId }
-                    AppResult.Success(album)
-                }
-
-                is AppResult.Error -> result
-                AppResult.Loading -> AppResult.Loading
+    operator fun invoke(albumId: AlbumId): Flow<AppResult<Album>> =
+        localDataSource.getAlbumById(albumId)
+            .map { album ->
+                album?.let { AppResult.Success(it) }
+                    ?: AppResult.Error(AlbumError.EmptyResponse)
             }
-        }
 }
