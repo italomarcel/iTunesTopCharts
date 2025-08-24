9489d7d6f9519c206fa9cbe26067ea4d836193d7
italo.santana <italo.santana@stone.com.br>
2025-08-24 06:39:09 -0300

    feat: add AlbumDetailScreen and integrate album detail navigation in App

--- composeApp/src/commonMain/kotlin/com/company/itunes/App.kt
+++ composeApp/src/commonMain/kotlin/com/company/itunes/App.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import coil3.ImageLoader
 import coil3.compose.setSingletonImageLoaderFactory
@@ -14,7 +12,9 @@ import coil3.network.ktor3.KtorNetworkFetcherFactory
 import coil3.request.crossfade
 import org.koin.compose.koinInject
 import presentation.ui.screen.AlbumsScreen
+import presentation.ui.screen.AlbumDetailScreen
 import presentation.viewmodel.AlbumsViewModel
+import domain.model.Album
 
 @Composable
 fun App() {
@@ -49,15 +49,23 @@ private fun AlbumsRoute(
     viewModel: AlbumsViewModel = koinInject()
 ) {
     val uiState by viewModel.uiState.collectAsState()
+    var selectedAlbum by remember { mutableStateOf<Album?>(null) }
 
-    AlbumsScreen(
-        uiState = uiState,
-        onSearchQueryChange = viewModel::updateSearchQuery,
-        onAlbumClick = { album ->
-            // TODO: Implement navigation
-        },
-        onRefresh = viewModel::refreshAlbums,
-        onRetry = viewModel::retryLoadAlbums,
-        modifier = modifier
-    )
+    if (selectedAlbum == null) {
+        AlbumsScreen(
+            uiState = uiState,
+            onSearchQueryChange = viewModel::updateSearchQuery,
+            onAlbumClick = { album -> selectedAlbum = album },
+            onRefresh = viewModel::refreshAlbums,
+            onRetry = viewModel::retryLoadAlbums,
+            modifier = modifier
+        )
+    } else {
+        selectedAlbum?.let { album ->
+            AlbumDetailScreen(
+                album = album,
+                onBack = { selectedAlbum = null }
+            )
+        }
+    }
 }
